967a834ff14592296d389e6dbd594658c9cc96a9
Masnoon Muztahid <2005067@ugrad.cse.buet.ac.bd>
2024-04-08 02:41:03 +0600

    Done Untill My skills

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings:["Backend Developer", "ML Enthusiast", "SWE Enthusiast"],
+    strings:["Backend Developer", "ML Enthusiast", "Software Engineering Learner"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
